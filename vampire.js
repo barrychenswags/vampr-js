d704553b3f8590b6465085f910e585d593f5783f
Barry <barry2014158@gmail.com>
2019-06-30 00:39:25 +0000

    implement vampire methods

--- vampire.js
+++ vampire.js
@@ -10,23 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    if (this.creator === null){
+      return 0;
+    }
+    return 1 + this.creator.numberOfVampiresFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal){
+      return true;
+    }
+    return false;
   }
+
 
   /** Stretch **/
 
@@ -36,7 +44,18 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    if(this.numberOfVampiresFromOriginal - vampire.numberOfVampiresFromOriginal > 0){ //this vampire is younger
+      this.creator.closestCommonAncestor(vampire);
+    }
+    else if(this.numberOfVampiresFromOriginal - vampire.numberOfVampiresFromOriginal === 0){
+      if(this === vampire){
+        return this;
+      }
+      this.creator.closestCommonAncestor(vampire.creator);
+    }
+    else{
+      vampire.creator.closestCommonAncestor(this);
+    }
   }
 }
 
